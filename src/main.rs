1c4bcaa839de4810e0adbf4f6f7f91a21c88c25a
0xh4ty <0xh4ty@proton.me>
2025-09-21 15:18:15 +0530

    refactor: split code into library crate and add search function stub

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs;
 use std::process;
 use std::error::Error;
+use minigrep::search;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,9 +11,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file path {}", config.file_path);
-
     if let Err(e) = run(config) {
         println!("Applicatin Error: {}", e);
         process::exit(1);
@@ -20,11 +18,11 @@ fn main() {
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
-
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 
